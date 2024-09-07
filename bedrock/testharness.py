b4dfc35f8a156012533a5000d6caed6cc5b582f2
Connor McLean <c.mclean165@gmail.com>
2024-09-07 12:52:54 -0600

    VS: use lancedb for vector store

--- bedrock/testharness.py
+++ bedrock/testharness.py
@@ -1,47 +1,63 @@
 import os
-import shutil
 from typing import Dict, Union
 from llama_index.core import (
     VectorStoreIndex,
     SimpleDirectoryReader,
     StorageContext,
-    load_index_from_storage,
 )
+from llama_index.vector_stores.lancedb import LanceDBVectorStore
 from llama_index.core.indices.base import BaseIndex
 from llama_index.core.base.base_query_engine import BaseQueryEngine
 from llama_index.llms.openai import OpenAI
 import logging
 import sys
 from dotenv import load_dotenv
+import concurrent.futures
+from typing import Type
 
+
+# project imports
 from agents import agents
+from agents.diligence import DueDiligenceAgent
 
 # load the .env file
 load_dotenv()
 
+# get the vector store uri from the .env file, if not set, use the default
+VECTOR_STORE_URI = os.getenv("VS_URI", "./lancedb")
 
-# 1. Document Ingestion and Vector Index Creation
+
+# Document Ingestion and Vector Index Creation
 def ingest_documents(data_room_path: str) -> Union[VectorStoreIndex, BaseIndex]:
-    PERSIST_DIR = "./storage"
-    if not os.path.exists(PERSIST_DIR):
-        documents = SimpleDirectoryReader(data_room_path).load_data()
-        llm = OpenAI(temperature=0.1, model="gpt-4o")
-        index = VectorStoreIndex.from_documents(documents, llm=llm)
-        index.storage_context.persist(persist_dir=PERSIST_DIR)
+    documents = SimpleDirectoryReader(data_room_path).load_data()
+    llm = OpenAI(temperature=0.1, model="gpt-4o")
 
-    else:
-        storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
-        index = load_index_from_storage(storage_context)
+    vector_store = LanceDBVectorStore(
+        uri=VECTOR_STORE_URI,
+        mode="overwrite",
+        table_name="documents",
+    )
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+    index = VectorStoreIndex.from_documents(
+        documents, storage_context=storage_context, llm=llm
+    )
 
     return index
 
 
-# 2. Query Engine Setup
+# Query Engine Setup
 def setup_query_engine(index: Union[VectorStoreIndex, BaseIndex]) -> BaseQueryEngine:
     return index.as_query_engine(response_mode="tree_summarize", similarity_top_k=5)
 
 
-# 3. Main Due Diligence Process
+def run_agent(agent: DueDiligenceAgent, output_dir: str):
+    agent_results = agent.analyze()
+    agent.write_to_markdown(agent_results, output_dir)
+    return agent.__class__.__name__, agent_results
+
+
+# Run the due diligence agents
 def run_due_diligence(
     data_room_path: str, output_dir: str
 ) -> Dict[str, Dict[str, str]]:
@@ -55,16 +71,19 @@ def run_due_diligence(
     if not diligence_agents:
         raise ValueError("No agents found")
 
-    results: Dict[str, Dict[str, str]] = {}
-    for agent in diligence_agents:
-        agent_results = agent.analyze()
-        results[agent.__class__.__name__] = agent_results
-        agent.write_to_markdown(agent_results, output_dir)
+    results = {}
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_to_agent = {
+            executor.submit(run_agent, agent, output_dir): agent
+            for agent in diligence_agents
+        }
+        for future in concurrent.futures.as_completed(future_to_agent):
+            agent_name, agent_results = future.result()
+            results[agent_name] = agent_results
 
     return results
 
 
-# Usage
 if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
